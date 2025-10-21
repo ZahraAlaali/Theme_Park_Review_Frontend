d37df5443ebfd61febc57ae598563761c76c1da9
jamsaeed <jameela.mohamed.saeed@gmail.com>
2025-10-21 21:05:59 +0300

    form complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import "./App.css"
+import axios from "axios"
+import Review from "./components/Review"
+import { useState, useEffect } from "react"
 
 const App = () => {
-  return <></>
+  const [reviews, setReviews] = useState()
+
+  useEffect(() => {
+    const getReviews = async () => {
+      try {
+        let response = await axios.get("http://localhost:3001/")
+        setReviews(response.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+    getReviews()
+  }, [])
+
+  return (
+    <>
+      <Review reviews={reviews} setReviews={setReviews} />
+      <h1>Issues:</h1>
+      {reviews?.map((review) => (
+        <div key={review._id}>
+          <h3>Type: {review.rateRide}</h3>
+          <p>Subject: {review.subject}</p>
+          <p>Review: {review.msg}</p>
+        </div>
+      ))}
+    </>
+  )
 }
 
 export default App
+
---
+++ src/components/Ride.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+import axios from "axios"
+
+const Ride = () => {
+
+
+const [rideState, setRideState] = useState()
+
+
+
+
+
+
+  return
+ <form action="">
+
+ </form>
+}
+
+export default Ride
